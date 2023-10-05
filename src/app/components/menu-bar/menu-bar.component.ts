f6674bc3f5aa3ad35ccb6f6c3bb23744cf1b2a02
GabrielRossatto <rossattogabriel@gmail.com>
2023-10-04 22:15:23 -0300

    atualização 'sobre'

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
-  constructor() { }
+
 
   ngOnInit(): void {
   }
